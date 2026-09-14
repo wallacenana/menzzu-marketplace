5292f3385b3ac01f9bc2e9c9b89e13363fb33eaa
wallacenana <walacenatanrodrigues@gmail.com>
2026-09-14 10:43:16 -0300

    Normalize cash payment orders

--- public-menu/index.php
+++ public-menu/index.php
@@ -1301,7 +1301,7 @@ try {
         </script>
 
         <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11" defer></script>
-                <script type="text/javascript" src="<?php echo esc_url(MENZZU_MARKETPLACE_URL . 'public-menu/script.js?v=2.53'); ?>" defer></script>
+                <script type="text/javascript" src="<?php echo esc_url(MENZZU_MARKETPLACE_URL . 'public-menu/script.js?v=2.54'); ?>" defer></script>
 
     </html>
 <?php
