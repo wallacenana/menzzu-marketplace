21944582a643923c3dda9ae9b8a3682c37ac06d7
wallacenana <walacenatanrodrigues@gmail.com>
2026-09-18 17:02:26 -0300

    Refine variation price badges

--- menzzu-marketplace.php
+++ menzzu-marketplace.php
@@ -3,7 +3,7 @@
 /**
  * Plugin Name: Menzzu Marketplace
  * Description: Marketplace Menzzu com descoberta de lojas, busca e catalogo.
- * Version: 3.1.143
+ * Version: 3.1.144
  * Author: Menzzu
  */
 
